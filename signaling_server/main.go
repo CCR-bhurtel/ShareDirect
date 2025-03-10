fd3bbb25db55772ca3942876bd0bbab497d05bd3
CCR-bhurtel <bhurtelshishir@gmail.com>
2025-03-10 12:57:25 +0545

    feat: enabled server to handle cors

--- signaling_server/main.go
+++ signaling_server/main.go
@@ -4,15 +4,26 @@ import (
 	"net/http"
 	"p2p-signaling-server/config"
 	server "p2p-signaling-server/server"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 	// Create a new signaling server
 	signalingServer := server.NewSignalingServer()
 
+	mux := http.NewServeMux()
+
 	// Handle websocket connections
 	http.HandleFunc("/ws", signalingServer.HandleWebSocketConnection)
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
 	// Start the server
-	http.ListenAndServe(":"+config.GetPort(), nil)
+	http.ListenAndServe(":"+config.GetPort(), c.Handler(mux))
 }
